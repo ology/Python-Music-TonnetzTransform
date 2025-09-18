6d534769f0c6b49c8a9bb79f8c59bcaf522f1afc
Gene Boggs <gene@ology.net>
2025-09-18 13:13:08 -0700

    WIP

--- README.md
+++ README.md
@@ -67,6 +67,30 @@ s.append(p)
 s.show()
 ```
 
+```python
+from music21 import duration, chord, stream
+from music_tonnetztransform import Transform
+from random_rhythms import Rhythm
+
+s = stream.Stream()
+p = stream.Part()
+
+r = Rhythm(durations=[1, 3/2, 2])
+motif = r.motif()
+
+t = Transform(max=len(motif))
+generated = t.circular()[0]
+print(generated)
+
+for i,dura in enumerate(motif):
+    c = chord.Chord(generated[i])
+    c.duration = duration.Duration(dura)
+    p.append(c)
+
+s.append(p)
+s.show()
+```
+
 ## SEE ALSO
 
 https://metacpan.org/pod/Music::NeoRiemannianTonnetz
--- tests.py
+++ tests.py
@@ -263,5 +263,23 @@ class TestTransform(unittest.TestCase):
         h = t.generate()[0]
         self.assertEqual(h[0], lpl[-1])
 
+    # def test_example(self):
+    #     from music21 import duration, chord, stream
+    #     from music_tonnetztransform import Transform
+    #     from random_rhythms import Rhythm
+    #     s = stream.Stream()
+    #     p = stream.Part()
+    #     r = Rhythm(durations=[1, 3/2, 2])
+    #     motif = r.motif()
+    #     t = Transform(max=len(motif))
+    #     generated = t.generate()[0]
+    #     print(generated)
+    #     for i,dura in enumerate(motif):
+    #         c = chord.Chord(generated[i])
+    #         c.duration = duration.Duration(dura)
+    #         p.append(c)
+    #     s.append(p)
+    #     s.show()
+
 if __name__ == '__main__':
   unittest.main()
